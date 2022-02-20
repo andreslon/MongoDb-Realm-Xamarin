7f7cbc98f68f0c323452fb299b2a35183a80feb7
Andrés Londoño <andreslon@outlook.com>
2022-02-20 01:38:09 -0500

    added base project with prism

---
+++ src/Xamarin.RealmApp/Xamarin.RealmApp/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Prism;
+using Prism.DryIoc;
+using Prism.Events;
+using Prism.Ioc;
+using Xamarin.RealmApp.ViewModels;
+using Xamarin.RealmApp.Views;
+
+namespace Xamarin.RealmApp
+{
+    public partial class App
+    {
+        /*
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor.
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ src/Xamarin.RealmApp/Xamarin.RealmApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.RealmApp.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+    }
+}
